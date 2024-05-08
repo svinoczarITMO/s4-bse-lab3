b778f586c58481ec0c85040e04e04d331e318350
svinoczar <alexsupper75@gmail.com>
2024-05-08 17:54:21 +0300

    init

---
+++ src/main/java/aca98b/web3lv2/HibernateUtil.java
@@ -0,0 +1,23 @@
+package aca98b.web3lv2;
+
+import jakarta.enterprise.context.SessionScoped;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
+@SessionScoped
+public class HibernateUtil {
+    private static final SessionFactory sessionFactory;
+
+    static {
+        Configuration configuration = new Configuration().configure();
+        configuration.addAnnotatedClass(HibernateElement.class);
+        ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();
+        sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+}
